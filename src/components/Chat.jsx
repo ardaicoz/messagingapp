6dafb8ac6e10f65b85addf9e36099984eb8fab1c
Arda İçöz <ardaicoz@gmail.com>
2022-11-22 01:38:36 +0300

    home page is complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from './Messages';
+import Input from "./Input";
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Mater</span>
+        <div className="chatIcons">
+          <img src={Add} alt=""/>
+          <img src={More} alt=""/>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://upload.wikimedia.org/wikipedia/en/f/f7/Mater_%28Cars%29.png" alt=""/>
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://upload.wikimedia.org/wikipedia/en/f/f7/Mater_%28Cars%29.png" alt=""/>
+      </div>
+    </div>
   )
 }
 
