f1d3c90993ba40f6ee2f0741262c5767b6563376
Can Avşar <can.avsar@ug.bilkent.edu.tr>
2022-11-26 22:46:17 +0300

    more design in messages page

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import React from "react";
 import Add from "../img/add.png";
 import More from "../img/more.png";
-import Messages from './Messages';
+import Messages from "./Messages";
 import Input from "./Input";
-import { useContext } from 'react';
-import { ChatContext } from '../context/ChatContext';
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
-  const { data } = useContext(ChatContext); 
+  const { data } = useContext(ChatContext);
 
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chatInfo">
-        <span>{data.user?.displayName}</span>
-        <div className="chatIcons">
-          <img src={Add} alt=""/>
-          <img src={More} alt=""/>
-        </div>
+        <span className="chatUsername">{data.user?.displayName}</span>
       </div>
-      <Messages/>
-      <Input/>
+      <Messages />
+      <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,8 +1,8 @@
-import { onSnapshot, doc } from 'firebase/firestore';
-import React from 'react'
-import { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../context/AuthContext';
-import { ChatContext } from '../context/ChatContext';
+import { onSnapshot, doc } from "firebase/firestore";
+import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
 
 const Chats = () => {
@@ -22,30 +22,32 @@ const Chats = () => {
       };
     };
 
-    currentUser.uid && getChats()
+    currentUser.uid && getChats();
   }, [currentUser.uid]);
 
   const handleSelect = (user) => {
-    dispatch({type: "CHANGE_USER", payload: user});
+    dispatch({ type: "CHANGE_USER", payload: user });
   };
 
   return (
-    <div className='chats'>
-      {Object.entries(chats)?.sort((a,b) => b[1].date - a[1].date).map((chat) => (
-        <div 
-          className='userChat' 
-          key={chat[0]} 
-          onClick={() => handleSelect(chat[1].userInfo)}
-        >
-          <img src={chat[1].userInfo.photoURL} alt=""/>
-          <div className='userChatInfo'>
-            <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].lastMessage?.text}</p>
+    <div className="chats">
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1].userInfo)}
+          >
+            <img src={chat[1].userInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
